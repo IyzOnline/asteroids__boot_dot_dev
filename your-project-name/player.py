dbae938c4159ec8636947182e81bf4fc0acd40ec
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-01-19 14:51:20 +0800

    feat: Create draw method for drawing player polygon

--- your-project-name/player.py
+++ your-project-name/player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, LINE_WIDTH
 import pygame
 
 class Player(CircleShape):
@@ -14,3 +14,6 @@ class Player(CircleShape):
     b = self.position - forward * self.radius - right
     c = self.position - forward * self.radius + right
     return [a, b, c]
+  
+  def draw(self, screen):
+    pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
