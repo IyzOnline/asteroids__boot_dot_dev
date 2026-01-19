01353448018eca5b1bd624a8f43d8101ac6bbb3b
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-01-19 14:55:36 +0800

    feat-fix: Create player instance in main and put screen fill and flip inside infinite loop

--- your-project-name/main.py
+++ your-project-name/main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,9 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
-
-    screen.fill("black")
-    screen.flip()
+        screen.fill("black")
+        player.draw(screen)
+        screen.flip()
 
 if __name__ == "__main__":
     main()
