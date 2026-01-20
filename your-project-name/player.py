e96dfded7e1c2f1b91919400dcedb6eb09a752bd
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-01-20 18:48:17 +0800

    feat: Create shoot method and activate whenever spacebar is pressed

--- your-project-name/player.py
+++ your-project-name/player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -32,6 +33,8 @@ class Player(CircleShape):
       self.move(dt)
     if keys[pygame.K_s]:
       self.move(-dt)
+    if keys[pygame.K_SPACE]:
+      self.shoot()
   
   def move(self, dt):
     #answer in course
@@ -42,3 +45,7 @@ class Player(CircleShape):
 
     #my answer
     self.position += pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SPEED * dt
+  
+  def shoot(self):
+    bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+    bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
